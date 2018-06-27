17322a6eccb839bdba9e47f00f596ee23e830cf1
Max Gonzih <gonzih@gmail.com>
2018-06-27 11:37:54 -0400

    Moving somewhere

--- main.go
+++ main.go
@@ -1,9 +1,67 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"syscall/js"
 )
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s\r\n", err)
+		panic(err)
+	}
+}
+
+type Component interface {
+	Render() error
+	RenderToString() (string, error)
+}
+
+type Props struct {
+}
+
+type GenericComponent struct {
+	props    Props
+	template *template.Template
+	targetID string
+}
+
+func (c *GenericComponent) Render() error {
+	html, err := c.RenderToString()
+	if err != nil {
+		return err
+	}
+
+	js.Global().Get("document").Call("getElementById", c.targetID).Set("innerHTML", html)
+	return nil
+}
+
+func (c *GenericComponent) RenderToString() (string, error) {
+	buf := new(bytes.Buffer)
+	err := c.template.Execute(buf, "")
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func NewComponent(templateID, targetID string) (Component, error) {
+	cmp := &GenericComponent{}
+	markup := js.Global().Get("document").Call("getElementById", templateID).Get("innerHTML").String()
+
+	tmpl, err := template.New("component").Parse(markup)
+
+	if err != nil {
+		return cmp, err
+	}
+
+	cmp.template = tmpl
+	cmp.targetID = targetID
+
+	return cmp, nil
+}
 
 func main() {
 	c := make(chan struct{}, 0)
@@ -14,6 +72,10 @@ func main() {
 
 	js.Global().Get("document").Call("getElementById", "myText").Call("addEventListener", "input", cb)
 	js.Global().Get("document").Call("getElementById", "runButton").Set("disabled", true)
+
+	cmp, err := NewComponent("helloTemplate", "root")
+	checkErr(err)
+	checkErr(cmp.Render())
 
 	<-c
 }
