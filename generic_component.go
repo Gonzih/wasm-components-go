7461126a9d3145718c0481758c6b15cc24a965f0
Max Gonzih <gonzih@gmail.com>
2018-06-29 09:34:03 -0400

    Attempt to use vdom

--- generic_component.go
+++ generic_component.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"syscall/js"
 
+	"github.com/albrow/vdom"
 	"github.com/google/uuid"
 )
 
@@ -12,35 +13,59 @@ type GenericComponent struct {
 	props       interface{}
 	propsFn     func(*GenericComponent) error
 	template    *template.Template
+	tree        *vdom.Tree
 	targetID    string
 	componentID string
+}
+
+func (c *GenericComponent) RenderToString() (string, error) {
+	return string(c.tree.HTML()), nil
 }
 
 func (c *GenericComponent) Render() error {
 	globalObserver.SetContext(c.componentID)
 
-	html, err := c.RenderToString()
+	err := c.propsFn(c)
+
 	if err != nil {
 		return err
 	}
 
-	js.Global().Get("document").Call("getElementById", c.targetID).Set("innerHTML", html)
-	return nil
-}
-
-func (c *GenericComponent) RenderToString() (string, error) {
-	err := c.propsFn(c)
-
-	if err != nil {
-		return "", err
-	}
-
 	buf := new(bytes.Buffer)
+
 	err = c.template.Execute(buf, c.props)
 	if err != nil {
-		return "", err
+		return err
 	}
-	return buf.String(), nil
+
+	newTree, err := vdom.Parse(buf.Bytes())
+	if err != nil {
+		return err
+	}
+
+	if c.tree != nil && len(c.targetID) > 0 {
+		// Calculate the diff between this render and the last render
+		// patches, err := vdom.Diff(c.tree, newTree)
+		// if err != nil {
+		// 	return err
+		// }
+
+		// Effeciently apply changes to the actual DOM
+		// root := js.Global().Get("document").Call("getElementById", c.targetID)
+		// if err := patches.Patch(root); err != nil {
+		// 	return err
+		// }
+	}
+
+	c.tree = newTree
+	root := js.Global().Get("document").Call("getElementById", c.targetID)
+	html, err := c.RenderToString()
+	if err != nil {
+		return err
+	}
+	root.Set("innerHTML", html)
+
+	return nil
 }
 
 func NewComponent(templateID, targetID string, propsFn func(*GenericComponent) error) (Component, error) {
