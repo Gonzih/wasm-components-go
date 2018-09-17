5c8f241a022e42468eae6c70d3b2497add131dae
Max Gonzih <gonzih@gmail.com>
2018-09-16 21:23:34 -0400

    Parse html in to a simple tree

--- parser_test.go
+++ parser_test.go
@@ -9,27 +9,77 @@ import (
 	"golang.org/x/net/html"
 )
 
-// TextToken
-// A StartTagToken looks like <a>.
-// StartTagToken
-// An EndTagToken looks like </a>.
-// EndTagToken
-// A SelfClosingTagToken tag looks like <br/>.
-// SelfClosingTagToken
-// A CommentToken looks like <!--x-->.
-// CommentToken
-// A DoctypeToken looks like <!DOCTYPE x>
-// DoctypeToken
+type El struct {
+	Tag         string
+	Attr        []html.Attribute
+	Content     string
+	Children    []*El
+	SelfClosing bool
+}
 
-func ConstructAnElement(token *html.Token, z *html.Tokenizer) {
+func (el *El) Print(level int) {
+	prefix := strings.Repeat("\t", level)
+	if el.SelfClosing {
+		log.Printf("%s<%s %v/>", prefix, el.Tag, el.Attr)
+	} else {
+		log.Printf("%s<%s %v>", prefix, el.Tag, el.Attr)
+		if len(el.Content) > 0 {
+			log.Printf("%s\t%s", prefix, el.Content)
+		}
+		for _, child := range el.Children {
+			child.Print(level + 1)
+		}
+		log.Printf("%s</%s>", prefix, el.Tag)
+	}
+}
 
+func ConstructAnElement(token *html.Token, tt html.TokenType, z *html.Tokenizer) *El {
+	parent := &El{}
+
+	parent.Tag = token.Data
+	parent.Attr = token.Attr
+
+	if tt != html.SelfClosingTagToken {
+		for {
+			tt := z.Next()
+			switch {
+			case tt == html.ErrorToken:
+				err := z.Err()
+				if err == io.EOF {
+					return parent
+				}
+				log.Printf("Error: %s", err)
+			case tt == html.StartTagToken:
+				t := z.Token()
+				child := ConstructAnElement(&t, tt, z)
+				parent.Children = append(parent.Children, child)
+			case tt == html.TextToken:
+				t := z.Token()
+				parent.Content = t.Data
+			case tt == html.EndTagToken:
+				break
+			case tt == html.SelfClosingTagToken:
+				t := z.Token()
+				child := ConstructAnElement(&t, tt, z)
+				parent.Children = append(parent.Children, child)
+			case tt == html.CommentToken:
+				break
+			case tt == html.DoctypeToken:
+				break
+			}
+		}
+	} else {
+		parent.SelfClosing = true
+	}
+
+	return parent
 }
 
 // walk a tree
 // contsructanelement should be called recursively
 
 func TestBasicHTMLParsing(t *testing.T) {
-	s := `<div><p><a href='https://some-link' @click='methodName' v:bind='data'>some text</a></p></div>`
+	s := `<div><p><a href='https://some-link' @click='methodName' v:bind='data' data-test=bebebe>some text</a></p><br/></div>`
 	r := strings.NewReader(s)
 	z := html.NewTokenizer(r)
 
@@ -44,14 +94,8 @@ func TestBasicHTMLParsing(t *testing.T) {
 			log.Fatal(err)
 		case tt == html.StartTagToken:
 			t := z.Token()
-			log.Print(t)
-
-			log.Println(t.Data)
-			log.Println(t.Attr)
-		case tt == html.EndTagToken:
-		case tt == html.SelfClosingTagToken:
-		case tt == html.CommentToken:
-		case tt == html.DoctypeToken:
+			el := ConstructAnElement(&t, tt, z)
+			el.Print(0)
 		}
 		break
 	}
